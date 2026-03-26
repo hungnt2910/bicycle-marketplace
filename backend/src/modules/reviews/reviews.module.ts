ca874eee40e854e3c8ce5a9e738574d58132f4af
CUONGBUII <cuongbb14@gmail.com>
2026-03-26 14:17:02 +0700

    Refactor review module: update Review entity, enhance CreateReviewDto, and implement CRUD operations in ReviewsController and ReviewsService

--- backend/src/modules/reviews/reviews.module.ts
+++ backend/src/modules/reviews/reviews.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { ReviewsService } from './reviews.service';
 import { ReviewsController } from './reviews.controller';
+import { Review, ReviewSchema } from 'src/entities/review.entity';
+import { MongooseModule } from '@nestjs/mongoose/dist/mongoose.module';
+
+import {
+  Transaction,
+  TransactionSchema,
+} from 'src/entities/transaction.entity';
+import { TransactionsModule } from '../transactions/transactions.module';
 
 @Module({
   providers: [ReviewsService],
-  controllers: [ReviewsController]
+  controllers: [ReviewsController],
+  imports: [
+    MongooseModule.forFeature([
+      { name: Review.name, schema: ReviewSchema },
+      { name: Transaction.name, schema: TransactionSchema },
+    ]),
+    TransactionsModule,
+  ],
 })
 export class ReviewsModule {}
--- backend/src/modules/reviews/reviews.service.ts
+++ backend/src/modules/reviews/reviews.service.ts
@@ -1,8 +1,58 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import { Model, Types } from 'mongoose';
+import { CreateReviewDto } from './dto/createReview.dto';
+import { Transaction, TransactionDocument } from 'src/entities';
+import { Review } from 'src/entities/review.entity'; // Import Review entity
 
 @Injectable()
 export class ReviewsService {
-  @InjectModel(ReviewsService.name) private reviewsModel: Model<any>;
+  @InjectModel(Review.name) private reviewsModel: Model<any>; // Sửa ở đây
+  @InjectModel(Transaction.name)
+  private transactionModel: Model<TransactionDocument>;
+
+  async createReview(data: CreateReviewDto) {
+    const reviewerId = data.reviewerId;
+
+    const checkTransaction = await this.transactionModel
+      .findOne({
+        buyerId: reviewerId,
+      })
+      .exec();
+
+    if (!checkTransaction) {
+      throw new Error('Transaction not found');
+    }
+
+    const existingReview = await this.reviewsModel.findOne({
+      reviewerId,
+    });
+
+    if (existingReview) {
+      throw new Error('You have already reviewed this transaction');
+    }
+
+    const createdReview = new this.reviewsModel(data);
+    return createdReview.save();
+  }
+
+  async getReviewsBySeller(sellerId: string) {
+    const sellerObjectId = sellerId;
+    return this.reviewsModel.find({ sellerId: sellerObjectId }).exec();
+  }
+
+  //thu hồi đánh giá
+  async deleteReview(id: string) {
+    return this.reviewsModel.findByIdAndDelete(id).exec();
+  }
+
+  // chỉnh sửa đánh giá
+  async updateReview(id: string, data: Partial<CreateReviewDto>) {
+    return this.reviewsModel.findByIdAndUpdate(id, data, { new: true }).exec();
+  }
+
+  // get tất cả các rating
+  async getAllRatings() {
+    return this.reviewsModel.find({}, 'rating').exec();
+  }
 }
